45f847393b72886859e2ed922b631c6cea278fef
NVision021 <nversic2@gmail.com>
2024-09-08 14:49:10 +1000

    Add event listeners and functions to numbers btns
    Allows user to input number onto the screen by clicking them and selects whether this is first or second number

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Functions for each operator
 const add = function(num1, num2) {
   return num1 + num2;
 }
@@ -14,6 +15,7 @@ const divide = function(num1, num2) {
   return num1 / num2;
 }
 
+//Function to determine which operator function to use
 const operate = function(num1, operator, num2) {
   if (operator === "+") {
     return add(num1, num2);
@@ -25,3 +27,34 @@ const operate = function(num1, operator, num2) {
     return divide(num1, num2);
   }
 }
+
+//Variable for each of the three components of a sum
+let number1 = "";
+let number2 = "";
+let operator = "";
+
+//create a reference to the calculator screen
+const screen = document.querySelector("#screen");
+
+//add event listeners to number buttons and display the appropriate one on screen
+const numberButtons = document.querySelectorAll(".button.number");
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    //check if the operator has been filled to determine whether it goes to 1 or 2
+    if (operator === "" && number1.length < 9) {
+      number1 += e.target.textContent;
+      console.log(number1);
+      screen.textContent = number1;
+    } else if (operator !== "" && number2.length < 9) {
+      number2 += e.target.textContent;
+      console.log(number2);
+      screen.textContent = number2;
+    }
+  });
+});
+
+// function that gets the operator
+
+// function that gets the "=" input and runs the sum if all the required items are present
+
+
